68aad0612b92f29e252dcc21b010a0c184222187
Emmanuel Ngufor <esngufor@myseneca.ca>
2024-04-09 18:01:30 -0400

    added project

---
+++ Project/MS1/Station.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef SENECA_STATION_H
+#define SENECA_STATION_H
+#include <string>
+
+namespace seneca
+{
+    class Station
+    {
+    private:
+        std::string m_name{};
+        std::string m_description{};
+        int m_id{};
+        unsigned int m_nextSerialNum{};
+        unsigned int m_numItems{};
+
+        // class variables
+        static size_t m_widthField;
+        static int id_generator; 
+
+    public:
+        Station() = default;
+        Station(const std::string& record); 
+        const std::string& getItemName() const;
+        size_t getNextSerialNumber();
+        size_t getQuantity() const;
+        void updateQuantity();
+        void display(std::ostream& os, bool full) const; 
+
+    };
+
+}  // namespace seneca
+
+#endif  // !SENECA_STATION_H
---
+++ Project/MS3/Workstation.cpp
@@ -0,0 +1,79 @@
+#include "Workstation.h"
+namespace seneca 
+{
+    std::deque<CustomerOrder> g_pending{};
+    std::deque<CustomerOrder> g_completed{};
+    std::deque<CustomerOrder> g_incomplete{};
+
+    Workstation::Workstation(const std::string& data) : Station{ data } {}
+
+    void Workstation::fill(std::ostream& os)
+    {
+        if (!m_orders.empty())
+        {
+            m_orders.front().fillItem(*this, os);
+        }
+    }
+
+    bool Workstation::attemptToMoveOrder() 
+    {
+        if (m_orders.empty()) 
+        {
+            return true;
+        }
+        else 
+        {
+            if (m_orders.front().isItemFilled(getItemName()) || this->getQuantity() == 0)
+            {
+                if (m_pNextStation == nullptr) 
+                {
+                    if (m_orders.front().isOrderFilled()) 
+                    {
+                        g_completed.emplace_back(std::move(m_orders.front()));
+                    }
+                    else 
+                    {
+                        g_incomplete.emplace_back(std::move(m_orders.front()));
+                    }
+                }
+                else 
+                {
+                    (*m_pNextStation) += std::move(m_orders.front());
+                }
+                m_orders.pop_front();
+
+                return true;
+            }
+            return false;
+        }
+    }
+
+    void Workstation::setNextStation(Workstation* station) {  m_pNextStation = station;  }
+
+    Workstation* Workstation::getNextStation() const { return m_pNextStation;  }
+
+    void Workstation::display(std::ostream& os) const
+    {
+        os << getItemName();
+        os << " --> ";
+
+        if (m_pNextStation == nullptr) 
+        {
+            os << "End of Line";
+        }
+        else 
+        {
+            os << m_pNextStation->getItemName();
+        }
+
+        os << std::endl;
+    }
+
+    Workstation& Workstation::operator+=(CustomerOrder&& newOrder) 
+    {
+        m_orders.emplace_back(std::move(newOrder));
+        return *this;
+    }
+
+    bool Workstation::empty() const { return m_orders.empty();   }
+}
